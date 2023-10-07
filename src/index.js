9a9be24683b7804983daae15c4c5ba9f34c93b10
Markswell <osoromarkswell80@gmail.com>
2023-10-07 23:06:07 +0300

    Creating Poster

--- src/index.js
+++ src/index.js
@@ -12,4 +12,29 @@ fetch("http://localhost:3000/films")
         movieList.appendChild(filmListItem); 
     })
 })
+// Make a GET request to retrieve movie data
+fetch("http://localhost:3000/films/1")
+.then(response => response.json())
+.then(movieData => {
+    const poster = document.getElementById("poster");
+    const title = document.getElementById("title");
+    const description = document.getElementById("description");
+    const runtime = document.getElementById("runtime");
+    const showtime = document.getElementById("showtime");
+    const ticketsAvailable = document.getElementById("tickets-available");
+
+    // Populate HTML elements with movie data
+    poster.src = movieData.poster;
+    title.textContent = "Title: " + movieData.title;
+    description.textContent = "Description: " + movieData.description;
+    runtime.textContent = "Runtime: " + movieData.runtime + " minutes";
+    showtime.textContent = "Showtime: " + movieData.showtime;
+    
+    // Calculate available tickets by subtracting tickets_sold from capacity
+    const availableTickets = movieData.capacity - movieData.tickets_sold;
+    ticketsAvailable.textContent = "Tickets Available: " + availableTickets;
+})
+.catch(error => {
+    console.error("Error fetching movie data:", error);
+});
 })
